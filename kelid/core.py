87a41bbde780cde7e0b7b744e6e008008e56242c
Ali Jafari <mmdddpdd@gmail.com>
2025-08-06 12:47:35 +0330

    Update core.py

--- kelid/core.py
+++ kelid/core.py
@@ -1,13 +1,19 @@
-import base64
 import marshal
+import base64
 
 def encode(code: str) -> str:
+    """
+    کد پایتون (رشته) رو می‌گیره و base64 رمزگذاری شده برمی‌گردونه
+    """
     compiled = compile(code, "<kelid>", "exec")
-    dumped = marshal.dumps(compiled)
-    encoded = base64.b64encode(dumped).decode()
+    marshaled = marshal.dumps(compiled)
+    encoded = base64.b64encode(marshaled).decode()
     return encoded
 
 def run(encoded_code: str):
-    dumped = base64.b64decode(encoded_code.encode())
-    code = marshal.loads(dumped)
+    """
+    رشته base64 رمز شده رو می‌گیره، اجرا می‌کنه
+    """
+    marshaled = base64.b64decode(encoded_code)
+    code = marshal.loads(marshaled)
     exec(code)
